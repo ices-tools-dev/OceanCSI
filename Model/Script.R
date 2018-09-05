7bcb6bcefb1249810df0e1daff3e1cb9a6cb1e8d
Hjalte Parner <hjalte@ices.dk>
2018-09-05 13:53:51 +0200

    Renamed searegions --> searegions, station --> stations, sample --> samples, stationsample --> stationSamples e.g. singular to plural.
    Added DIN coalesce function to summarize DIN correctly
    
    Moved DIN indicator downwards

--- Model/Script.R
+++ Model/Script.R
@@ -11,8 +11,8 @@ ipak(packages)
 
 # Input files ------------------------------------------------------------------
 
-#coastlineFile <- "Input/Country_Europe_Extended.shp"
-coastlineFile <- "Input/EEA_Coastline_20170228.shp"
+coastlineFile <- "Input/Country_Europe_Extended.shp"
+#coastlineFile <- "Input/EEA_Coastline_20170228.shp"
 searegionFile <- "Input/EEA_SeaRegion_20180831.shp"
 stationFile <- "Input/OceanCSI_Station_20180829.txt"
 sampleFile <- "Input/OceanCSI_Sample_20180829.txt"
@@ -26,65 +26,66 @@ coastline <- sf::st_read(coastlineFile)
 #sf::st_is_valid(coastline)
 
 # Make geometries valid by doing the buffer of nothing trick
-coastline <- sf::st_buffer(coastline, 0.0)
+#coastline <- sf::st_buffer(coastline, 0.0)
 
 # Transform projection into UTM33N
 coastline <- sf::st_transform(coastline, crs = 32633)
 
 # Make 1km buffer
-coastline_Within1km <- sf::st_buffer(coastline, 1000)
+#coastline_Within1km <- sf::st_buffer(coastline, 1000)
 
 # Make 20km buffer
 coastline_Within20km <- sf::st_buffer(coastline, 20000)
+#plot(coastline_Within20km)
 
-# Regions -------------------------------------------------------------------
+# SeaRegions -------------------------------------------------------------------
 
 # Read shapefile
-searegion <- sf::st_read(searegionFile)
+searegions <- sf::st_read(searegionFile)
 
 # Check if geometries is valid
-#sf::st_is_valid(searegion)
+#sf::st_is_valid(searegions)
 
 # Make geometries valid by doing the buffer of nothing trick
-#searegion <- sf::st_buffer(searegion, 0.0)
+#searegions <- sf::st_buffer(searegions, 0.0)
 
 # Transform projection into UTM33N
-searegion <- sf::st_transform(searegion, crs = 32633)
+searegions <- sf::st_transform(searegions, crs = 32633)
 
 # Stations ---------------------------------------------------------------------
 
 # Read stations
-#station <- fread(input = stationFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
-station <- fread(input = stationFile, sep = "\t", nrows = 100000, na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
+stations <- fread(input = stationFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
+#stations <- fread(input = stationFile, sep = "\t", nrows = 100000, na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
 
 # Make stations spatial keeping original latitude/longitude
-station <- sf::st_as_sf(station, coords = c("Longitude", "Latitude"), remove = FALSE, crs = 4326)
+stations <- sf::st_as_sf(stations, coords = c("Longitude", "Latitude"), remove = FALSE, crs = 4326)
 
 # Project stations into UTM33N
-station <- sf::st_transform(station, crs = 32633)
-station$UTM_E <- sf::st_coordinates(station)[,1]
-station$UTM_N <- sf::st_coordinates(station)[,2]
-station <- sf::st_transform(station, crs = 4326)
-station$Lon <- sf::st_coordinates(station)[,1]
-station$Lat <- sf::st_coordinates(station)[,2]
-station <- sf::st_transform(station, crs = 32633)
-#station <- sf::st_set_geometry(station, NULL)
+stations <- sf::st_transform(stations, crs = 32633)
+stations$UTM_E <- sf::st_coordinates(stations)[,1]
+stations$UTM_N <- sf::st_coordinates(stations)[,2]
+stations <- sf::st_transform(stations, crs = 4326)
+stations$Lon <- sf::st_coordinates(stations)[,1]
+stations$Lat <- sf::st_coordinates(stations)[,2]
+stations <- sf::st_transform(stations, crs = 32633)
 
 # Classify stations into sea regions - the R way. However the stations have allready been classified when extracting the data from the database
-#station$SeaRegionID_R <- sf::st_intersects(station, searegion) %>% as.numeric()
-#table(station$SeaRegionID_R)
-#table(station$SeaRegionID)
+#stations$SeaRegionID_R <- sf::st_intersects(stations, searegions) %>% as.numeric()
+#table(stations$SeaRegionID_R)
+#table(stations$SeaRegionID)
 
 # Classify stations into on land
-#station$OnLand <- apply(sf::st_intersects(station, coastline, sparse = TRUE), 1, any)
+#stations$OnLand <- apply(sf::st_intersects(stations, coastline, sparse = TRUE), 1, any)
 
 # Classify stations into within 1km from land
-#station$Within1km <- apply(sf::st_intersects(station, coastline_Within1km, sparse = TRUE), 1, any)
-#station$Within1km <- apply(sf::st_is_within_distance(station, coastline, 1000, sparse = TRUE), 1, any)
+#stations$Within1km <- apply(sf::st_intersects(stations, coastline_Within1km, sparse = TRUE), 1, any)
 
 # Classify stations into within 20km from land
-station$Within20km <- apply(sf::st_intersects(station, coastline_Within20km, sparse = TRUE), 1, any)
-#station$Within20km <- apply(sf::st_is_within_distance(station, coastline, 20000, sparse = TRUE), 1, any)
+stations$Within20km <- apply(sf::st_intersects(stations, coastline_Within20km, sparse = TRUE), 1, any)
+
+# Remove spatial column in order to merge station samples
+stations <- sf::st_set_geometry(stations, NULL)
 
 # Classify stations using square assignment
 #
@@ -94,45 +95,27 @@ station$Within20km <- apply(sf::st_intersects(station, coastline_Within20km, spa
 # from the coastline (m = 80) and 5.5 km for open water station more than 20 km away from the coastline (m = 20).
 # The procedure does not totally prevent errorneous aggregation of data belonging to stations close to each other or errorneous
 # breakup of time series into fragments due to small shifts in position, but reduces the problem considerably.
-station$m <- 20
-#station$m <- ifelse(station$Within20km, 80, 20)
-station$iY <- round(station$Latitude*station$m)
-station$latitude_center <- station$iY/Station$m
-station$rK <- station$m/cos(station$latitude_center*atan(1)/45)
-station$iX <- round(station$Longitude*station$rK)
-station$longitude_center <- station$iX/station$rK
-comb <- with(station, paste(iX, iY))
-station$ClusterID <- match(comb, unique(comb))
+#station$m <- 20
+stations$m <- ifelse(stations$Within20km, 80, 20)
+stations$iY <- round(stations$Latitude*stations$m)
+stations$latitude_center <- stations$iY/stations$m
+stations$rK <- stations$m/cos(stations$latitude_center*atan(1)/45)
+stations$iX <- round(stations$Longitude*stations$rK)
+stations$longitude_center <- stations$iX/stations$rK
+comb <- with(stations, paste(iX, iY))
+stations$ClusterID <- match(comb, unique(comb))
 
 # Samples ----------------------------------------------------------------------
 
 # Read samples
-sample <- fread(sampleFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
+samples <- fread(sampleFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE)
 
 # StationSamples ----------------------------------------------------------
 
-# merge station and samples
+# merge stations and samples
 setkey(stations, StationID)
 setkey(samples, StationID, SampleID)
-stationsamples <- stations[samples]
-
-# Dissolved Inorganic Nitrogen - DIN (Winter) ----------------------------------
-#   Parameters: [NO3-N] + [NO2-N] + [NH4-N]
-#   Depth: <= 10
-#   Period: Winter
-#     January - March for stations within Baltic Sea east of 15 E
-#     January - February for other stations
-#   Aggregation Method: Arithmetric mean of mean by station and cluster per year
-
-# Filter stations rows and columns --> ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate, Nitrite, Ammonium
-wk <- StationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrate|Nitrite|Ammonium) & (NitrateQ != 3 & NitrateQ != 4 & NitriteQ != 3 & NitriteQ != 4 & AmmoniumQ != 3 & AmmoniumQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate, Nitrite, Ammonium)]
-wk$DIN <- apply(wk[, c("Nitrate", "Nitrite", "Ammonium")], 1, coalesce)
-
-# Calculate station mean --> ClusterID, StationID, Latitude, Longitude, Year, MinDepth, MaxDepth, AvgTemperature, AvgSalinity, AvgDIN, MinDIN, MaxDIN, CountSamples
-wk1 <- wk[, list(MinDepth = min(Depth), MaxDepth = max(Depth), AvgTemperature = mean(Temperature), AvgSalinity = mean(Salinity), AvgDIN = mean(DIN), MinDIN = min(DIN), MaxDIN = max(DIN), SampleCount = .N), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year)]
-
-# Calculate cluster mean --> SeaRegionID, ClusterID, AvgLatitude, AvgLongitude, Year, MinMinDepth, MaxMaxDepth, AvgAvgAvgTemperature, AvgAvgSalinity, AvgAvgDIN, MinMinDIN, MaxMaxDIN, SumCountSamples
-wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), MinDepth = min(MinDepth), MaxDepth = max(MaxDepth), AvgTemperature = mean(AvgTemperature), AvgSalinity = mean(AvgSalinity), AvgDIN = mean(AvgDIN), MinDIN = min(MinDIN), MaxDIN = max(MaxDIN), SampleCount = sum(SampleCount)), list(SeaRegionID, ClusterID, Year)]
+stationSamples <- stations[samples]
 
 # Nitrate (Winter) -------------------------------------------------------
 #   Parameters: [NO3-N]
@@ -143,7 +126,7 @@ wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), 
 #   Aggregation Method: Arithmetric mean of mean by station and cluster per year
 
 # Filter stations rows and columns --> ClusterID, StationID, Year, Depth, Temperature, Salinity, Nitrate
-wk <- StationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrate) & (NitrateQ != 3 & NitrateQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate)]
+wk <- stationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrate) & (NitrateQ != 3 & NitrateQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate)]
 
 # Calculate station mean --> ClusterID, StationID, Year, MinDepth, MaxDepth, AvgTemperature, AvgSalinity, AvgNitrate, MinNitrate, MaxNitrate, CountSamples
 wk1 <- wk[, list(MinDepth = min(Depth), MaxDepth = max(Depth), AvgTemperature = mean(Temperature), AvgSalinity = mean(Salinity), AvgNitrate = mean(Nitrate), MinNitrate = min(Nitrate), MaxNitrate = max(Nitrate), SampleCount = .N), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year)]
@@ -160,7 +143,7 @@ wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), 
 #   Aggregation Method: Arithmetric mean of mean by station and cluster per year
 
 # Filter stations rows and columns --> ClusterID, StationID, Year, Depth, Temperature, Salinity, Nitrite
-wk <- StationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrite) & (NitriteQ != 3 & NitriteQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrite)]
+wk <- stationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrite) & (NitriteQ != 3 & NitriteQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrite)]
 
 # Calculate station mean --> ClusterID, StationID, Year, MinDepth, MaxDepth, AvgTemperature, AvgSalinity, AvgNitrate, MinNitrite, MaxNitrite, CountSamples
 wk1 <- wk[, list(MinDepth = min(Depth), MaxDepth = max(Depth), AvgTemperature = mean(Temperature), AvgSalinity = mean(Salinity), AvgNitrite = mean(Nitrite), MinNitrite = min(Nitrite), MaxNitrite = max(Nitrite), SampleCount = .N), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year)]
@@ -177,13 +160,32 @@ wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), 
 #   Aggregation Method: Arithmetric mean of mean by station and cluster per year
 
 # Filter stations rows and columns --> ClusterID, StationID, Year, Depth, Temperature, Salinity, Ammonium
-wk <- StationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Ammonium) & (AmmoniumQ != 3 & AmmoniumQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Ammonium)]
+wk <- stationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Ammonium) & (AmmoniumQ != 3 & AmmoniumQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Ammonium)]
 
 # Calculate station annual average --> ClusterID, StationID, Year, MinDepth, MaxDepth, AvgTemperature, AvgSalinity, AvgAmmonium, MinAmmonium, MaxAmmonium, CountSamples
 wk1 <- wk[, list(MinDepth = min(Depth), MaxDepth = max(Depth), AvgTemperature = mean(Temperature), AvgSalinity = mean(Salinity), AvgAmmonium = mean(Ammonium), MinAmmonium = min(Ammonium), MaxAmmonium = max(Ammonium), SampleCount = .N), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year)]
 
 # Calculate cluster annual average --> ClusterID, Year, MinMinDepth, MaxMaxDepth, AvgAvgAvgTemperature, AvgAvgSalinity, AvgAvgAmmonium, MinMinAmmonium, MaxMaxAmmonium, SumCountSamples
 wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), MinDepth = min(MinDepth), MaxDepth = max(MaxDepth), AvgTemperature = mean(AvgTemperature), AvgSalinity = mean(AvgSalinity), AvgAmmonium = mean(AvgAmmonium), MinAmmonium = min(MinAmmonium), MaxAmmonium = max(MaxAmmonium), SampleCount = sum(SampleCount)), list(SeaRegionID, ClusterID, Year)]
+
+# Dissolved Inorganic Nitrogen - DIN (Winter) ----------------------------------
+#   Parameters: [NO3-N] + [NO2-N] + [NH4-N]
+#   Depth: <= 10
+#   Period: Winter
+#     January - March for stations within Baltic Sea east of 15 E
+#     January - February for other stations
+#   Aggregation Method: Arithmetric mean of mean by station and cluster per year
+
+# Filter stations rows and columns --> ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate, Nitrite, Ammonium
+wk <- stationSamples[Depth <= 10 & ifelse(SeaRegionID == 1 & Longitude > 15, Month >= 1 & Month <= 3, Month >= 1 & Month <= 2) & !is.na(Nitrate|Nitrite|Ammonium) & (NitrateQ != 3 & NitrateQ != 4 & NitriteQ != 3 & NitriteQ != 4 & AmmoniumQ != 3 & AmmoniumQ != 4), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year, Depth, Temperature, Salinity, Nitrate, Nitrite, Ammonium)]
+coalesce <- function(x) if (all(is.na(x))) NA else sum(x, na.rm = TRUE)
+wk$DIN <- apply(wk[, c("Nitrate", "Nitrite", "Ammonium")], 1, coalesce)
+
+# Calculate station mean --> ClusterID, StationID, Latitude, Longitude, Year, MinDepth, MaxDepth, AvgTemperature, AvgSalinity, AvgDIN, MinDIN, MaxDIN, CountSamples
+wk1 <- wk[, list(MinDepth = min(Depth), MaxDepth = max(Depth), AvgTemperature = mean(Temperature), AvgSalinity = mean(Salinity), AvgDIN = mean(DIN), MinDIN = min(DIN), MaxDIN = max(DIN), SampleCount = .N), list(SeaRegionID, ClusterID, StationID, Latitude, Longitude, Year)]
+
+# Calculate cluster mean --> SeaRegionID, ClusterID, AvgLatitude, AvgLongitude, Year, MinMinDepth, MaxMaxDepth, AvgAvgAvgTemperature, AvgAvgSalinity, AvgAvgDIN, MinMinDIN, MaxMaxDIN, SumCountSamples
+wk2 <- wk1[, list(AvgLatitude = mean(Latitude), AvgLongitude = mean(Longitude), MinDepth = min(MinDepth), MaxDepth = max(MaxDepth), AvgTemperature = mean(AvgTemperature), AvgSalinity = mean(AvgSalinity), AvgDIN = mean(AvgDIN), MinDIN = min(MinDIN), MaxDIN = max(MaxDIN), SampleCount = sum(SampleCount)), list(SeaRegionID, ClusterID, Year)]
 
 # TN [NTOT] indicator (Winter) --------------------------------------------
 
